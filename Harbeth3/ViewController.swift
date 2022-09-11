8b3c7efe26462e01f656b30d7ca82c5108c4ff5c
BRIKSING <briksing@yandex.ru>
2022-09-11 19:19:20 +0500

    Initial Commit

---
+++ Harbeth3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Harbeth3
+//
+//  Created by Anna Zhikhareva on 11.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
